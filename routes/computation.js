976beb9ee4f15ea469ff22b5f16480e9761713c1
jamsheeraslam <jamsheeraslam4269@gmail.com>
2023-10-30 10:41:02 -0500

    modified all files

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var x= Math.floor(Math.random());
-var y=Math.floor(Math.random());
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var x= Math.random().toFixed(2);
+var y=Math.random().toFixed(2);
 var result1=Math.atan(x);
 var result2=Math.exp(y);
   res.send(`Math.atan() applied to[${x}]=${result1}<br>Math.exp() applied to[${y}]=${result2}`);
